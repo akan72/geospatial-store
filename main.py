05b019a617fd61554104ea28b545ef6f5dd5d9f6
Alex Kan <lexokan@gmail.com>
2020-01-18 17:00:40 -0500

    Add dotenv logic and Werkzeug version

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from app import app
 
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Load pickled model here? 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
