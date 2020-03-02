8002a38427b53da2d608e9b45fb853b0800c77c6
Alex Kan <lexokan@gmail.com>
2020-03-02 11:14:25 -0500

    Add correct raw data path to test_requests.py

--- flask/test_requests.py
+++ flask/test_requests.py
@@ -8,24 +8,18 @@ data = {'petal_width': 2}
 
 r = requests.post(iris_url, json=data)
 
-print(r.status_code)
-print(r.text)
-
+# print(r.status_code)
+# print(r.text)
 
 planet_url = addr + '/upload_file_api'
-content_type = 'image/jpeg'
-
-headers = {'content-type': content_type} 
-
-# files = {'file': open('src/app/static/uploads/test_11.jpg', 'rb')}
 
 files = {
-    'file1': open('src/app/static/uploads/test_11.jpg', 'rb'),
-    'file2': open('src/app/static/uploads/test_14.jpg', 'rb')
+    'file1': open('data/raw/test-jpg/test_11.jpg', 'rb'),
+    'file2': open('data/raw/test-jpg/test_14.jpg', 'rb')
     }   
  
 r = requests.post(planet_url, files=files)
 print(r.status_code)
 print(r.text)
 
-# curl -F "file=@src/app/static/uploads/test_11.jpg" http://localhost:5000/upload_file_api
+# curl -F "file=@data/raw/test-jpg/test_11.jpg" http://localhost:5000/upload_file_api
