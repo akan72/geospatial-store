e509c56178618883c5f5d2baccbe6a8958fdfc4c
Alex Kan <lexokan@gmail.com>
2020-04-18 19:01:58 -0400

    Add config files into react src

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CONFIG } from './config.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        players: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(CONFIG.API_BASE_URL)
+        .then(results => results.json())
+        .then(players => this.setState({players: players}));
+  }
+
+  render() {
+    const players = this.state.players.map((player, index) => <li key={index}>{player.lastname} {player.firstname}</li>);
+
+    return (
+      <div>
+          <h1>Players list</h1>
+          <ul>
+            {players}
+          </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
