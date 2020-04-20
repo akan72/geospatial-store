85505970820c6266a5a583ece5ed6bc3f7bb21fe
Nikhil Vytla <nvawesomeguy@gmail.com>
2020-04-19 23:22:08 -0400

    Deleted redundancies, updated routes, styling #1

--- flask/src/app/__init__.py
+++ flask/src/app/__init__.py
@@ -1,6 +1,22 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = 'src/app/static/uploads/'
+
+# from src.app import views
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = 'src/app/static/uploads/'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
 
 from src.app import views
+from .views import main
+
+app.register_blueprint(main)
