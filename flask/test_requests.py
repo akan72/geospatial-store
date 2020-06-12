7ef694ee9d67946d6c9a807b5640abb8565d0d61
Alex Kan <lexokan@gmail.com>
2020-06-12 16:19:02 -0400

    Modify test_requests.py to allow for request to Docker api

--- flask/test_requests.py
+++ flask/test_requests.py
@@ -2,21 +2,16 @@ import os
 import json
 import requests 
 
-addr = 'http://localhost:5000'
-
-planet_url = addr + '/upload_image_api'
-
 files = [
     ('file', open('data/raw/test-jpg/test_11.jpg', 'rb')),
     ('file', open('data/raw/test-jpg/test_14.jpg', 'rb'))
     ]
 
+addr = 'http://localhost'   
+planet_url = addr + '/upload_image_api'
 r = requests.post(planet_url, files=files)
 
 print(r.text)
 print(r.status_code)
 
-# r = requests.post(addr+'/predict_petal_length_api', data={'petal_width': 1})
-# print(r.text)
-
-# curl -F "file=@data/raw/test-jpg/test_11.jpg" http://localhost:5000/upload_image_api
+# curl -F "file=@data/raw/test-jpg/test_11.jpg" http://localhost/upload_image_api
